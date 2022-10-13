da1da9e7db7ce470cee77b0f733dc46ac74b6393
codeaktivist <codeaktivist@gmail.com>
2022-10-13 17:53:44 +0200

    Stage 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,31 +2,51 @@ import { useState } from 'react'
 
 
 const App = () => {
-    const [person, setPerson] = useState(['Martin'])
+    const [person, setPerson] = useState([
+        { name: 'Arto Hellas', number: '040-123456', id: 1 },
+        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }])
     const [newName, setNewName] = useState('')
+    const [newNumber, setNewNumber] = useState('')
 
-    const onChangeHandler = (event) =>
+    const onChangeNameHandler = (event) =>
         setNewName(event.target.value)
+
+    const onChangeNumberHandler = (event) =>
+        setNewNumber(event.target.value)
 
     const onSubmitHandler = (event) => {
         event.preventDefault()
-        person.includes(newName) ? alert(`${newName} is already in the phonebook`) : setPerson(person.concat(newName))
+        person.some((person) => person.name === newName) ? alert(`${newName} is already in the phonebook`) : setPerson(person.concat({
+            name: newName,
+            number: newNumber,
+            id: person.length + 1
+        }))
         setNewName('')
+        setNewNumber('')
     }
 
     return (
     <>
     <h2>Phonebook</h2>
     <form onSubmit={onSubmitHandler}>
-        <input
-            value={newName}
-            onChange={onChangeHandler}/>
+        <div>
+            name: <input
+                value={newName}
+                onChange={onChangeNameHandler}/>
+        </div>
+        <div>
+            number: <input
+                value={newNumber}
+                onChange={onChangeNumberHandler}/>
+        </div>
         <button type='submit'>Add</button>
     </form>
     <h2>Numbers</h2>
     <table>
         <tbody>
-            {person.map((person) => <tr key={person}><td>{person}</td></tr>)}
+            {person.map((person) => <tr key={person.id}><td>{person.name}</td><td>{person.number}</td></tr>)}
         </tbody>
     </table>
     </>
