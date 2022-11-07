039b7a0b385125163ed5cd7d9223eefa1b82d4ba
codeaktivist <codeaktivist@gmail.com>
2022-11-07 23:27:26 +0100

    Stage 4.4
    total likes test

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,7 +1,16 @@
 const dummy = (blogs) => {
-    return true
+    return blogs
+        ? true
+        : false
+}
+
+const totalLikes = (blogs) => {
+    return blogs.reduce((sum, current) => {
+        return sum + current.likes
+    }, 0)
 }
 
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
