f16a39cbb0ee1a6391d679ff6285230f5b0c41a7
codeaktivist <codeaktivist@gmail.com>
2022-11-08 09:40:50 +0100

    Stage 4.5
    favorite blog function and test

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -6,11 +6,29 @@ const dummy = (blogs) => {
 
 const totalLikes = (blogs) => {
     return blogs.reduce((sum, current) => {
+        console.log('ADD: ', current)
+        console.log('SUM: ', sum + current.likes)
         return sum + current.likes
     }, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    const favorite = blogs.reduce((best, current) => {
+        console.log('BEST: ', best.likes)
+        console.log('CURRENT: ', current.likes)
+        return (current.likes >= best.likes)
+            ? current
+            : best
+    }, { likes: 0 })
+    return {
+        title: favorite.title,
+        author: favorite.author,
+        likes: favorite.likes
+    }
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
