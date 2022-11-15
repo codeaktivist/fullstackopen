43477ed6409bdd3ad67e427bdb75f76bae28131a
codeaktivist <codeaktivist@gmail.com>
2022-11-15 10:09:35 +0100

    Stage 4.18
    jwt for auth implemented

---
+++ part4/bloglist/controllers/login.js
@@ -0,0 +1,22 @@
+const loginRouter = require('express').Router()
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+
+loginRouter.post('/', async (request, response) => {
+    const { username, password } = request.body
+
+    const userExists = await User.findOne({ username })
+    if (!userExists) {
+        return response.status(403).json({ error: 'user not found' })
+    }
+
+    if (!await bcrypt.compare(password, userExists.passwordHash)) {
+        return response.status(403).json({ error: 'wrong password' })
+    }
+
+    const token = await jwt.sign({ username, id: userExists._id }, process.env.SECRET, { expiresIn: 60 * 60 })
+    response.json({ token, username, name: userExists.name })
+})
+
+module.exports = loginRouter
