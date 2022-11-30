aa91c8589825ed3a11fed2e861543bc0890e8f18
codeaktivist <codeaktivist@gmail.com>
2022-11-30 16:19:08 +0100

    Stage 5.18
    test login with right and wrong credentials

--- part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
+++ part5/bloglist-frontend/cypress/e2e/blog_app.cy.js
@@ -1,9 +1,26 @@
 describe('Blog app', () => {
+    const newUser = {
+        name: 'Martin',
+        username: 'martin',
+        password: 'asdf'
+    }
+
     beforeEach(() => {
+
         cy.request('POST', 'http://localhost:3003/api/testing/reset')
             .then(response => {
                 expect(response.status).eq(204)
             })
+        cy.request({
+            method: 'POST',
+            url: 'http://localhost:3003/api/users',
+            body: newUser
+        })
+            .then(response => {
+                cy.log(response)
+                expect(response.body.username).eq(newUser.username)
+            })
+
         cy.visit('http://localhost:3000/')
     })
     it('login form is shown', () => {
@@ -15,5 +32,35 @@ describe('Blog app', () => {
             })
         cy.contains('Username:')
         cy.contains('Password:')
+    })
+
+    describe('Login', () => {
+        it('succeeds with correct credentials', () => {
+            cy.contains('Username')
+                .find('input')
+                .as('inputUsername')
+                .type(newUser.username)
+            cy.contains('Password:')
+                .find('input')
+                .type(newUser.password)
+            cy.get('button')
+                .click()
+            cy.contains(`Logged-in as ${newUser.name}`)
+        })
+
+        it('fails with wrong credentials', () => {
+            cy.contains('Username')
+                .find('input')
+                .as('inputUsername')
+                .type(newUser.username)
+            cy.contains('Password:')
+                .find('input')
+                .type('wrong')
+            cy.get('button')
+                .click()
+            cy.contains('wrong password')
+                .should('have.css', 'border-color', 'rgb(255, 0, 0)')     
+                .and('have.class', 'notification warning')     
+        })
     })
 })
