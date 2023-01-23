57a1d8954389709fb8398080f0ad3db054e2168c
codeaktivist <codeaktivist@gmail.com>
2023-01-23 18:43:52 +0100

    Stage 6.13
    fetch from json-server

--- part6/anecdotes-redux/src/App.js
+++ part6/anecdotes-redux/src/App.js
@@ -3,7 +3,22 @@ import Filter from './components/Filter'
 import AnecdoteList from './components/AnecdoteList'
 import AnecdoteForm from './components/AnecdoteForm'
 
+import noteService from './services/anecdotes'
+import { appendAnecdote } from './reducers/anecdoteReducer'
+import { useDispatch } from 'react-redux'
+import { useEffect } from 'react'
+
 const App = () => {
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        noteService.getAll().then(notes => {
+            notes.map(note =>
+                dispatch(appendAnecdote(note))
+            )
+        })
+    }, [dispatch])
+
     return (
         <div>
             <h2>Anecdotes</h2>
