8d0e0ac6f2e3b79d0d6db5e11796ebade0788cb4
codeaktivist <codeaktivist@gmail.com>
2023-02-01 10:49:53 +0100

    Stage 6.19 (new)
    new exercises, now using React Query

---
+++ part6/anecdotes-query/src/App.js
@@ -0,0 +1,54 @@
+import axios from 'axios'
+import { useQuery } from 'react-query'
+import AnecdoteForm from './components/AnecdoteForm'
+import Notification from './components/Notification'
+
+const App = () => {
+
+    const handleVote = (anecdote) => {
+        console.log('vote')
+    }
+
+    const result = useQuery('anecdotes', () => axios
+        .get('http://localhost:3001/anecdotes')
+        .then(res => res.data))
+
+    if (result.isLoading) {
+        console.log(result);
+        return (
+            <div>loading data ...</div>
+        )
+    }
+
+    if (result.isError) {
+        console.log(result);
+        return (
+            <div>server problems</div>
+        )
+    }
+
+    const anecdotes = result.data
+
+    return (
+        <div>
+            <h3>Anecdote app</h3>
+
+            <Notification />
+            <AnecdoteForm />
+
+            {anecdotes.map(anecdote =>
+                <div key={anecdote.id}>
+                    <div>
+                        {anecdote.content}
+                    </div>
+                    <div>
+                        has {anecdote.votes}
+                        <button onClick={() => handleVote(anecdote)}>vote</button>
+                    </div>
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default App
