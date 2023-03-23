9364009bd0d858f2ca98a56259fa881dd3b97aa2
codeaktivist <codeaktivist@gmail.com>
2023-03-23 11:05:23 +0100

    Stage 7.6

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -57,24 +57,24 @@ const Anecdote = ({ anecdote }) => {
 };
 
 const CreateNew = (props) => {
-  const content = useField("text");
-  const author = useField("text");
-  const info = useField("text");
+  const {reset: contentReset, ...contentProps} = useField("text");
+  const {reset: authorReset, ...authorProps} = useField("text");
+  const {reset: infoReset, ...infoProps} = useField("text");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     props.addNew({
-      content: content.value,
-      author: author.value,
-      info: info.value,
+      content: contentProps.value,
+      author: authorProps.value,
+      info: infoProps.value,
       votes: 0,
     });
   };
 
   const resetForm = () => {
-    content.reset();
-    author.reset();
-    info.reset();
+    contentReset();
+    authorReset();
+    infoReset();
   };
 
   return (
@@ -83,15 +83,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input {...content} />
+          <input {...contentProps} />
         </div>
         <div>
           author
-          <input {...author} />
+          <input {...authorProps} />
         </div>
         <div>
           url for more info
-          <input {...info} />
+          <input {...infoProps} />
         </div>
         <button type="submit">create</button>
         <button type="reset" onClick={() => resetForm()}>reset</button>
